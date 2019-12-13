9557919b7a8980c114d68160d10df4671827e80c
pingjin <736252868@qq.com>
2019-12-13 11:51:18 +0800

    0701

---
+++ src/com/anthonyzero/medium/_0701/Solution.java
@@ -0,0 +1,18 @@
+package com.anthonyzero.medium._0701;
+
+import com.anthonyzero.tools.TreeNode;
+
+public class Solution {
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else if (val > root.val) {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
