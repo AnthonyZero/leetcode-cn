22c7624480809786dea824a405cbb5e507c98dbf
anthonyzero <736252868@qq.com>
2020-02-19 15:54:32 +0800

    455

--- notes/0455/README.md
+++ notes/0455/README.md
@@ -40,18 +40,19 @@
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-
         int res = 0;
-        int start = 0;
-        for (int i = 0; i < g.length; i++) {
+        int gi = 0;
+        int si = 0;
 
-            for(;start < s.length; start++) {
-                if(s[start] >= g[i]) {
-                    //饼干大小能满足
-                    start++; //从下一块饼干开始
-                    res++;
-                    break; //跳出当前循环 看下个孩子
-                }
+        while(gi < g.length && si < s.length) {
+            if (s[si] >= g[gi]) {
+                //当前饼干尺寸能满足当前小朋友胃口
+                res++;
+                gi++;
+                si++;
+            } else {
+                //当前饼干尺寸不满足 看下块饼干是否满足
+                si++;
             }
         }
         return res;
--- src/com/anthonyzero/easy/_0455/Solution.java
+++ src/com/anthonyzero/easy/_0455/Solution.java
@@ -4,28 +4,49 @@ import java.util.Arrays;
 
 public class Solution {
 
+//    public int findContentChildren(int[] g, int[] s) {
+//        Arrays.sort(g);
+//        Arrays.sort(s);
+//
+//        int res = 0;
+//        int start = 0;
+//        for (int i = 0; i < g.length; i++) {
+//
+//            for(;start < s.length; start++) {
+//                if(s[start] >= g[i]) {
+//                    //饼干大小能满足
+//                    start++; //从下一块饼干开始
+//                    res++;
+//                    break; //跳出当前循环 看下个孩子
+//                }
+//            }
+//        }
+//        return res;
+//    }
+
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-
         int res = 0;
-        int start = 0;
-        for (int i = 0; i < g.length; i++) {
+        int gi = 0;
+        int si = 0;
 
-            for(;start < s.length; start++) {
-                if(s[start] >= g[i]) {
-                    //饼干大小能满足
-                    start++; //从下一块饼干开始
-                    res++;
-                    break; //跳出当前循环 看下个孩子
-                }
+        while(gi < g.length && si < s.length) {
+            if (s[si] >= g[gi]) {
+                //当前饼干尺寸能满足当前小朋友胃口
+                res++;
+                gi++;
+                si++;
+            } else {
+                //当前饼干尺寸不满足 看下块饼干
+                si++;
             }
         }
         return res;
     }
 
     public static void main(String[] args) {
-        int[] g = new int[]{1,2};
+        int[] g = new int[]{2,3};
         int[] s = new int[]{1,2,3};
         System.out.println(new Solution().findContentChildren(g, s));
     }
