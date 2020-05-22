9e6bc26709fcb2d8d0b999094ae7f113cbea7914
pingjin <736252868@qq.com>
2020-05-22 14:16:52 +0800

    0783

--- src/com/anthonyzero/easy/_0783/Solution.java
+++ src/com/anthonyzero/easy/_0783/Solution.java
@@ -8,30 +8,58 @@ import java.util.List;
 
 public class Solution {
 
-    public List<Integer> list = new ArrayList<>();
+//    public List<Integer> list = new ArrayList<>();
+//    public int minDiffInBST(TreeNode root) {
+//        if(root == null) {
+//            return -1;
+//        }
+//        inOrder(root);
+//        int minDiff = Integer.MAX_VALUE;
+//        for(int i = 1; i < list.size(); i++) {
+//            //相邻元素的 差的绝对值
+//            int differ = Math.abs(list.get(i) - list.get(i - 1));
+//            if(differ < minDiff) {
+//                minDiff = differ;
+//            }
+//        }
+//        return minDiff;
+//    }
+//
+//    //中序遍历 从小到到
+//    private void inOrder(TreeNode root) {
+//        if(root == null) {
+//            return;
+//        }
+//        inOrder(root.left);
+//        list.add(root.val);
+//        inOrder(root.right);
+//    }
+
+    public Integer preElement; //遍历的过程中 前一个元素
+    public int minDiff = Integer.MAX_VALUE;
     public int minDiffInBST(TreeNode root) {
         if(root == null) {
             return -1;
         }
         inOrder(root);
-        int minDiff = Integer.MAX_VALUE;
-        for(int i = 1; i < list.size(); i++) {
-            //相邻元素的 差的绝对值
-            int differ = Math.abs(list.get(i) - list.get(i - 1));
-            if(differ < minDiff) {
-                minDiff = differ;
-            }
-        }
         return minDiff;
     }
 
-    //中序遍历 从小到到
+    // 中序遍历
     private void inOrder(TreeNode root) {
         if(root == null) {
             return;
         }
         inOrder(root.left);
-        list.add(root.val);
+        if(preElement == null) {
+            preElement = root.val;
+        } else {
+            int differ = Math.abs(root.val - preElement);
+            if (differ < minDiff) {
+                minDiff = differ;
+            }
+            preElement = root.val;
+        }
         inOrder(root.right);
     }
 
