d9f259d2bde75a19bdafce2ac9c483ff195f0c5c
anthonyzero <736252868@qq.com>
2020-08-03 15:43:09 +0800

    0231

---
+++ src/com/anthonyzero/easy/_0231/Solution.java
@@ -0,0 +1,25 @@
+package com.anthonyzero.easy._0231;
+
+public class Solution {
+
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0) {
+            return false;
+        }
+        if(n == 1 || n == 2) {
+            return true;
+        } else {
+            //n > 2情况 继续整除下去 或者 不能除以2了
+            if (n % 2 == 0) {
+                return isPowerOfTwo(n / 2);
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isPowerOfTwo(16));
+        System.out.println(new Solution().isPowerOfTwo(0));
+    }
+}
