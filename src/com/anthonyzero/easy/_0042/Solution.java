3224237decaade6b2e827334113b94ef6fcd0c3c
anthonyzero <736252868@qq.com>
2020-08-12 15:15:18 +0800

    0560

---
+++ src/com/anthonyzero/easy/_0042/Solution.java
@@ -0,0 +1,28 @@
+package com.anthonyzero.easy._0042;
+
+public class Solution {
+
+    public int maxSubArray(int[] nums) {
+        if(nums.length == 0) {
+            return 0;
+        }
+        int minArrSub = 0; //前缀和最小值
+        int res = nums[0]; //前缀和最大值
+        int sum = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            sum += nums[i];
+            minArrSub = Integer.min(sum - nums[i], minArrSub);
+            if(minArrSub < 0) {
+                res = Integer.max(sum - minArrSub, res);
+            } else {
+                res = Integer.max(sum, res);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+        System.out.println(new Solution().maxSubArray(new int[]{-2,-1}));
+    }
+}
